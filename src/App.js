5d79b9f8eefe574177da2a2dc4a103968ca93802
Toya Rosuello <victoria.rosuello@gmail.com>
2020-07-27 23:17:53 -0700

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Calendar from "./calendar";
-
-const App = () => {
-  return <Calendar />;
-};
-
-ReactDOM.render(<App />, document.getElementById("root"));
