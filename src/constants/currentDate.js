4f33a1e5e5ec86a481468a747fc0adb7af37c5c8
Toya Rosuello <victoria.rosuello@gmail.com>
2020-07-27 22:09:15 -0700

    Revert "Updates"
    This reverts commit 6b3ae24a6d4b27518deca1441fd5a97971d10e9e.

---
+++ src/constants/currentDate.js
@@ -0,0 +1,7 @@
+export const currentDate = {
+  year: 1214,
+  month: "Ki'afu",
+  day: 14,
+};
+
+export default currentDate;
---
+++ src/constants/weekdays.js
@@ -0,0 +1,11 @@
+const WEEKDAYS = [
+  "Itimui",
+  "Ka'ula",
+  "Ka'ahel",
+  "Ka'ilo",
+  "Ka'unti",
+  "Ka'ugha",
+  "Ka'ali",
+];
+
+export default WEEKDAYS;
