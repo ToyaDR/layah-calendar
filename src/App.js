12cc55c7539a2f9b3afd9a8306d5ea770b1f7043
Toya Rosuello <victoria.rosuello@gmail.com>
2020-07-27 21:23:10 -0700

    Base calendar

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Calendar from "./calendar";
+
+const App = () => {
+  return <Calendar />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
